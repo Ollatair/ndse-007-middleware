cca0042421ee77a07c7395c73f1473bfef7ac83c
Andrey Tikhonenkov <ollatair@gmail.com>
2023-10-01 15:50:20 +0300

    fix typos

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const { v4: uuid } = require('uuid');
 
-const user = require('./routes/user')
-const book = require('./routes/book')
-
-
+const user = require('./routes/user');
+const book = require('./routes/book');
 
 const app = express();
 app.use(express.json());
 app.use('/api/books', user);
 app.use('/api/books', book);
 
-
-
- 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+
+const router = express.Router();
 
 router.post('/api/user/login', (req, res) => {
-    res.status(200);
-    res.json({ id: 1, mail: 'test@mail.ru' });
-  });
+  res.status(200);
+  res.json({ id: 1, mail: 'test@mail.ru' });
+});
 
-
-  module.exports = router
+module.exports = router;
